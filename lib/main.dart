4751be4d6b3da7132c980e961bffbc79ac8b24d2
Faizaan <faizaan.cse@gmail.com>
2023-04-15 20:59:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,331 @@
+//import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Profile(),
+    theme: ThemeData(fontFamily: 'Poppins'),
+  ));
+}
+
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter = counter + 1;
+          });
+        },
+        child: Container(
+          width: 60,
+          height: 60,
+          child: Icon(Icons.add),
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+              )),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                flex: 5,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(255, 20, 17, 201),
+                        Colors.deepPurpleAccent
+                      ],
+                    ),
+                  ),
+                  child: Column(children: [
+                    SizedBox(
+                      height: 110.0,
+                    ),
+                    CircleAvatar(
+                      radius: 65.0,
+                      backgroundImage: AssetImage('assets/sky.jpeg'),
+                      // backgroundColor: Colors.white,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text('Faizaan',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        )),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'Android Developer',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Expanded(
+                flex: 5,
+                child: Container(
+                  color: Colors.grey[200],
+                  child: Center(
+                      child: Card(
+                          margin: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+                          child: Container(
+                              width: 310.0,
+                              height: 290.0,
+                              child: Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Profile",
+                                      style: TextStyle(
+                                        fontSize: 17.0,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                    Divider(
+                                      color: Colors.grey[300],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          Icons.home,
+                                          color: Colors.blueAccent[400],
+                                          size: 35,
+                                        ),
+                                        SizedBox(
+                                          width: 20.0,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Home",
+                                              style: TextStyle(
+                                                fontSize: 15.0,
+                                              ),
+                                            ),
+                                            Text(
+                                              "India",
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: Colors.grey[400],
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          Icons.auto_awesome,
+                                          color: Colors.yellowAccent[400],
+                                          size: 35,
+                                        ),
+                                        SizedBox(
+                                          width: 20.0,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Interest",
+                                              style: TextStyle(
+                                                fontSize: 15.0,
+                                              ),
+                                            ),
+                                            Text(
+                                              "App Development and Flutter",
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: Colors.grey[400],
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          Icons.favorite,
+                                          color: Colors.pinkAccent[400],
+                                          size: 35,
+                                        ),
+                                        SizedBox(
+                                          width: 20.0,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Loves",
+                                              style: TextStyle(
+                                                fontSize: 15.0,
+                                              ),
+                                            ),
+                                            Text(
+                                              "Traveling",
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: Colors.grey[400],
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Icon(
+                                          Icons.people,
+                                          color: Colors.lightGreen[400],
+                                          size: 35,
+                                        ),
+                                        SizedBox(
+                                          width: 20.0,
+                                        ),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Team",
+                                              style: TextStyle(
+                                                fontSize: 15.0,
+                                              ),
+                                            ),
+                                            Text(
+                                              "Myself",
+                                              style: TextStyle(
+                                                fontSize: 12.0,
+                                                color: Colors.grey[400],
+                                              ),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              )))),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+              top: MediaQuery.of(context).size.height * 0.45,
+              left: 20.0,
+              right: 20.0,
+              child: Card(
+                  child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        child: Column(
+                      children: [
+                        Text(
+                          'Tap',
+                          style: TextStyle(
+                              color: Colors.grey[400], fontSize: 14.0),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "$counter",
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        )
+                      ],
+                    )),
+                    Container(
+                      child: Column(children: [
+                        Text(
+                          'Institute',
+                          style: TextStyle(
+                              color: Colors.grey[400], fontSize: 14.0),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          'IIIT-R',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        )
+                      ]),
+                    ),
+                    Container(
+                        child: Column(
+                      children: [
+                        Text(
+                          'Branch',
+                          style: TextStyle(
+                              color: Colors.grey[400], fontSize: 14.0),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          'CSE',
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        )
+                      ],
+                    )),
+                  ],
+                ),
+              )))
+        ],
+      ),
+    );
+  }
+}
